590c0bf6ac45abe5bfb80db28ec220a1d5177d3b
Ali Aliyyan <85094417+ali-sypher@users.noreply.github.com>
2025-08-20 20:27:41 -0700

    Update about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -135,30 +135,30 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+                Hello! My name is Ali Aliyyan and I’m passionate about building modern, scalable applications and
+                exploring cloud technologies. My journey into software development began when I started working
+                on personal projects and open-source contributions, and since then I’ve been focused on creating
+                impactful digital solutions.
             </p>
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
+              <a href="https://zagmersol.com/">a merchant service provider</a>,{' '}
+              <a href="https://www.tipsiti.com/">a curator brand</a>,{' '}
+              <a href="https://www.thesmartnexus.com">a big software house</a>, and{' '}
+              <a href="https://upwork.com/">as a freelancer</a>. My
               main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
+              experiences at <a href="https://upwork.com/">as a freelancer on upwork</a> for a variety of
               clients.
             </p>
 
             <p>
               I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
+              <a href="https://www.linkedin.com/in/ali-a-7b7306189">
+                completed a large sacle web scrapping project.
               </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              that can source data from social media platform regardless of the restrictions
+              &amp; by the platform.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
