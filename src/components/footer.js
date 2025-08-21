7f8ba6af5d1dd3746450e7e5d5edd764e2d8038a
Ali Aliyyan <85094417+ali-sypher@users.noreply.github.com>
2025-08-20 22:03:26 -0700

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -105,8 +105,8 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
-          <div>Designed &amp; Built by Brittany Chiang</div>
+        <a href="https://github.com/ali-sypher">
+          <div>Designed &amp; Built by Brittany Chiang revamped by Ali Aliyyan</div>
 
           {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
